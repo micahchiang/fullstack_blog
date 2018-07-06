7949c0f02a5093ee30afca063cc551563d2499e4
Micah Chiang <micahkchiang@gmail.com>
2018-07-06 09:31:56 -0700

    added header to list view

--- user/src/components/client/BlogList.vue
+++ user/src/components/client/BlogList.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="blog__container">
+    <header class="blog__container-header">
+      <h2 class="blog__container-header-title">
+        Hello and welcome to my blog!
+      </h2>
+      <p class="blog__container-header-text">
+        This site was built from the ground up with Vue. Honestly it's probably a little gimpy, but, it's a continual work in progress.
+        Below you'll find (hopefully) find some entries that may have to do with software engineering, or some other topic. If you don't find any entries,
+        then it probably means I've broken something and am trying to fix it. Thanks for visiting!
+      </p>
+    </header>
     <ul class="entries__list">
       <li class="entry__title" v-for="(entry, index) in entries" :key=index>
         <p class="entry__date">{{entry.date}}</p>
@@ -28,6 +38,33 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+}
+
+.blog__container-header {
+  padding: 2rem;
+  width: 80%;
+  border-bottom: 3px solid $primary-color;
+  @media only screen and (max-width: 400px) {
+    width: 95%;
+  }
+
+  &-title {
+    font-size: 4rem;
+    font-weight: 100;
+    margin: 1rem 0;
+    @media only screen and (max-width: 400px) {
+      font-size: 1.8rem;
+    }
+  }
+
+  &-text {
+    font-size: 2rem;
+    font-weight: 100;
+    @media only screen and (max-width: 400px) {
+      font-size: 1.3rem;
+    }
+  }
 }
 
 .entries__list {
